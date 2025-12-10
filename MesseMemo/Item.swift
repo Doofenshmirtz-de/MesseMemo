601cfd62f4a66258b3bad6e851bf8f1c5156ddb4
Jarno <Jarno.Kibies@outlook.de>
2025-12-10 18:32:21 +0100

    Initial Commit

---
+++ MesseMemo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MesseMemo
+//
+//  Created by Jarno Kibies on 10.12.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
